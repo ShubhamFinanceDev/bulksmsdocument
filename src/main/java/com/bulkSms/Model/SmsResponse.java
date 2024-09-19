63c1f7f54b44a0bdcfebad4fc1b016d70808bde1
shagun <shagun.kaushik@dbalounge.com>
2024-09-19 19:02:06 +0530

    add pagination and modify response add extra field nextPage

--- src/main/java/com/bulkSms/Model/SmsResponse.java
+++ src/main/java/com/bulkSms/Model/SmsResponse.java
@@ -7,20 +7,19 @@ import java.util.List;
 @Data
 public class SmsResponse {
 
-    private int totalCount;
+    private long totalCount;
 
     private String msg;
 
+    private boolean nextPage;
+
     private List<Object> smsInformation;
 
-    public SmsResponse(int totalCount, String msg, List<Object> smsInformation){
+    public SmsResponse(long totalCount, boolean nextPage, String msg, List<Object> smsInformation){
         this.totalCount = totalCount;
+        this.nextPage = nextPage;
         this.msg = msg;
         this.smsInformation = smsInformation;
-    }
-
-    public SmsResponse(String msg){
-        this.msg = msg;
     }
 
 }
--- src/main/java/com/bulkSms/Repository/DataUploadRepo.java
+++ src/main/java/com/bulkSms/Repository/DataUploadRepo.java
@@ -1,6 +1,7 @@
 package com.bulkSms.Repository;
 
 import com.bulkSms.Entity.DataUpload;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,10 +11,10 @@ import java.util.Optional;
 public interface DataUploadRepo extends JpaRepository<DataUpload, Long> {
 
     @Query("select d from DataUpload d where d.certificateCategory = :userCategory and d.smsFlag = 'N'")
-    List<DataUpload> findByCategoryAndSmsFlagNotSent(String userCategory);
+    List<DataUpload> findByCategoryAndSmsFlagNotSent(String userCategory, Pageable pageable);
 
     @Query("select d from DataUpload d where d.certificateCategory = :smsCategory and d.smsFlag = 'Y'")
-    List<DataUpload> findBySmsCategory(String smsCategory);
+    List<DataUpload> findBySmsCategoryOfSendSms(String smsCategory, Pageable pageable);
 
     @Query("SELECT COUNT(c) FROM DataUpload c WHERE c.smsFlag = 'Y'")
     Long getSmsCount();
@@ -23,4 +24,25 @@ public interface DataUploadRepo extends JpaRepository<DataUpload, Long> {
 
     @Query("select d from DataUpload d where d.smsFlag = 'Y'")
     List<DataUpload> findByType();
+
+    @Query("select d from DataUpload d where d.smsFlag = 'Y'")
+    List<DataUpload> findByTypeOfSendSms(Pageable pageable);
+
+    @Query("select d from DataUpload d where d.smsFlag = 'N'")
+    List<DataUpload> findByTypeForUnsendSms(Pageable pageable);
+
+    @Query("select d from DataUpload d where d.certificateCategory = :smsCategory and d.smsFlag = 'N'")
+    List<DataUpload> findBySmsCategoryForUnsendSms(String smsCategory, Pageable pageable);
+
+    @Query("select count(d) from DataUpload d where d.smsFlag = 'Y'")
+    long findCount();
+
+    @Query("select count(d) from DataUpload d where d.certificateCategory = :smsCategory and d.smsFlag = 'Y'")
+    long findCountWithSmsCategory(String smsCategory);
+
+    @Query("select count(d) from DataUpload d where d.smsFlag = 'N'")
+    long findUnsendSmsCountByType();
+
+    @Query("select count(d) from DataUpload d where d.certificateCategory = :smsCategory and d.smsFlag = 'N'")
+    long findUnsendSmsCountByCategory(String smsCategory);
 }
