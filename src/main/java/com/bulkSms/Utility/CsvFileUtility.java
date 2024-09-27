28da0959c93b600684d122171bc9eca0d8f42a37
Atishra <atish.rai@dbalounge.com>
2024-09-27 16:20:06 +0530

    changes in csv logic

--- src/main/java/com/bulkSms/Utility/CsvFileUtility.java
+++ src/main/java/com/bulkSms/Utility/CsvFileUtility.java
@@ -1,9 +1,11 @@
 package com.bulkSms.Utility;
 import com.bulkSms.Entity.BulkSms;
 import com.bulkSms.Entity.DataUpload;
+import com.bulkSms.Repository.DataUploadRepo;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.BufferedReader;
@@ -11,10 +13,14 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
-public class CsvFileUtility{
+public class CsvFileUtility {
     public static String TYPE = "text/csv";
+
+    @Autowired
+    private DataUploadRepo dataUploadRepo;
 
     public boolean hasCsvFormat(MultipartFile file) {
         return TYPE.equals(file.getContentType());
@@ -27,13 +33,23 @@ public class CsvFileUtility{
              CSVParser csvParser = new CSVParser(bReader, CSVFormat.DEFAULT.withDelimiter('|').withTrim())) {
             List<CSVRecord> csvRecords = csvParser.getRecords();
             for (CSVRecord record : csvRecords) {
-                DataUpload dataUpload = new DataUpload();
-                dataUpload.setLoanNumber(record.get(0));
-                dataUpload.setMobileNumber(record.get(1));
-                dataUpload.setCertificateCategory(record.get(2));
-                dataUploadList.add(dataUpload);
+                String loanNumber = record.get(0);
+                Optional<DataUpload> existingDataUpload = dataUploadRepo.findByloanNumber(loanNumber);
+
+                if (existingDataUpload.isPresent()) {
+                    DataUpload dataUpload = existingDataUpload.get();
+                    dataUpload.setSmsFlag("N");
+                    dataUploadRepo.save(dataUpload);
+                } else {
+                    DataUpload dataUpload = new DataUpload();
+                    dataUpload.setLoanNumber(record.get(0));
+                    dataUpload.setMobileNumber(record.get(1));
+                    dataUpload.setCertificateCategory(record.get(2));
+                    dataUpload.setSmsFlag(record.get(3));
+                    dataUploadList.add(dataUpload);
+                }
             }
+            return dataUploadList;
         }
-        return dataUploadList;
     }
 }
