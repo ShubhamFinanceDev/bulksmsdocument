e8222ffa0e8a082e01a12d2745e4f66a2cddde38
nainish <pmo_dbalounge@shubham.co>
2024-10-14 10:35:13 +0530

    fix api template

--- src/main/java/com/bulkSms/Utility/SmsUtility.java
+++ src/main/java/com/bulkSms/Utility/SmsUtility.java
@@ -36,7 +36,7 @@ public class SmsUtility {
 
     int year = Year.now().getValue();
 
-    private String adhocMessage = "Dear+Customer,+Congratulations+to+be+part+of+the+Shubham+family,+we+are+pleased+to+share+your+welcome+kit+having+welcome+letter,+repayment+schedule+%26+sanction+letter+cum+MITC.+Kindly+download+your+welcome+kit+from+below+link.+For+any+enquiry+related+to+this,+you+can+call+at+our+customer+care+toll+free+no.+-+1800+258+2225+or+email+at+customercare@shubham.co%0aLink:- ";
+    private String adhocMessage = "Dear%20Customer%2C%20Congratulations%20to%20be%20part%20of%20the%20Shubham%20family%2C%20we%20are%20pleased%20to%20share%20your%20welcome%20kit%20having%20welcome%20letter%2C%20repayment%20schedule%20%26%20sanction%20letter%20cum%20MITC.%20Kindly%20download%20your%20welcome%20kit%2C%20fair%20practice%20code%2C%20%26%20exclusion%20list%20from%20below%20links.%20For%20any%20enquiry%20related%20to%20this%2C%20you%20can%20call%20at%20our%20customer%20care%20toll%20free%20no.%20-%201800%20258%202225%20or%20email%20at%20customercare%40shubham.co%0AWelcome%20kit%20Link%3A- ";
     private String soaTemplate = "Dear+Customer,%0aPlease+download+your+Yearly+Statement+of+Account+from+below+link+for+the+period+of+01-Apr-" + (year - 1) + "+to+31-Mar-" + year + ".%0aRegards%0aShubham+Housing+Development+Finance+Company+Ltd%0aLink: ";
     private String interestCertificateTemplate = "Dear+Customer,%0aPlease+download+your+Yearly+Interest+Certificate+from+below+link+for+the+period+of+01-Apr-" + (year - 1) + "+to+31-Mar-" + year + ".%0aRegards%0aShubham+Housing+Development+Finance+Company+Ltd%0aLink: ";
     private String reminderPayment = "";
